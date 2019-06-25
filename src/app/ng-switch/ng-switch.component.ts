f2d7efa61337b4ff2a0e94f2f391b7967a3d6c07
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-25 16:04:40 +0900

    add ng-switch

---
+++ src/app/ng-switch/ng-switch.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch',
+  templateUrl: './ng-switch.component.html',
+  styleUrls: ['./ng-switch.component.css']
+})
+export class NgSwitchComponent implements OnInit {
+  public myVar: string = 'B';
+  public text: string = `
+  <span [ngSwitch]="myVar">
+    <p *ngSwitchCase="'A'">
+      This is A
+    </p>
+    <p *ngSwitchCase="'B'">
+      This is B
+    </p>
+    <p *ngSwitchDefault>
+      This is something else
+    </p>
+  </span>
+  `;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
