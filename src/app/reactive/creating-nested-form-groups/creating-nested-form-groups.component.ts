045d375b92c4d65411923f25667f2ccdcccb984f
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-27 14:13:14 +0900

    fixed creating-nested

--- src/app/reactive/creating-nested-form-groups/creating-nested-form-groups.component.ts
+++ src/app/reactive/creating-nested-form-groups/creating-nested-form-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-creating-nested-form-groups',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creating-nested-form-groups.component.css']
 })
 export class CreatingNestedFormGroupsComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+  });
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
   }
 
 }
